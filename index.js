791c6c11585944bf98244d4ab07ca0478f9f482d
Tymon Kwiatkowski <rudytyms@gmail.com>
2025-09-29 07:32:57 +0000

    Added icon and html

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 import { createServer } from 'node:http';
 import { URL } from "node:url";
 
@@ -17,3 +18,47 @@ const host = "localhost";
 server.listen(port, host, () => {
     console.log(`Server listening on http://${host}:${port}`);
 });
+*/
+
+import http from "node:http";
+import fs from "node:fs";
+
+const port = 8000;
+
+const index_html = fs.readFileSync("public/index.html");
+const favicon_ico = fs.readFileSync("public/favicon.ico");
+
+const server = http.createServer(function (req, res) {
+  if (req.url && req.url === "/") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html");
+    res.end(index_html);
+  }
+
+  if (req.url && req.url === "/favicon.ico") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "image/vnd.microsoft.icon");
+    res.end(favicon_ico);
+  }
+
+  if (req.url && req.url === "/json") {
+    const content = {
+      message: "Hello there",
+      time: Intl.DateTimeFormat("pl", {
+        dateStyle: "full",
+        timeStyle: "long",
+        timeZone: "CET",
+      }).format(Date.now()),
+    };
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify(content));
+  }
+
+  // Unknown request
+  res.statusCode = 404;
+  res.end();
+});
+
+server.listen(port);
+console.log(`Server listening on port http://localhost:${port}`);
